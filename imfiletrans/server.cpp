7e3595112e06768a0fecea2e589b3a8f89643e71
dedoz <d3d0zz@gmail.com>
2013-06-07 20:17:54 +0400

    working server

--- imfiletrans/server.cpp
+++ imfiletrans/server.cpp
@@ -1,3 +1,7 @@
+#include<sys/types.h>
+#include<sys/time.h>
+#include<sys/resource.h>
+#include<sys/wait.h>
 #include<sys/socket.h>
 #include<netdb.h>
 #include<stdlib.h>
@@ -8,7 +12,9 @@
 #include<map>
 #include<vector>
 
-const int BUF_SIZE = 2048;
+const int BUF_SIZE = 32*1024*1024;
+const int MAX_TAG_LEN = 4;
+int ID = 0;
 
 #define PORT "3111"
 
@@ -23,17 +29,71 @@ struct s_buffer {
 using namespace std;
 
 vector<int> fds;
+vector<pollfd> polls;
+map<int, s_buffer*> buffers;
+vector<bool> senders;
+vector<int> ids;
 
-int prpagate_data(pollfd* src, s_buffer* buf, pollfd* dst){
 
+int pull_data(pollfd* in, s_buffer* buf){
+    printf("starting pull: %d %d\n", in, buf);
+    if(buf->in_died) return 0;
+    if(buf->pos < BUF_SIZE && in->revents & POLLIN){
+        int cnt = read(in->fd, buf->data + buf->pos, BUF_SIZE - buf->pos);
+        if(cnt <= 0){
+            perror("read() failed");
+            buf->in_died = true;
+            return 0;
+        }
+        else{
+            buf->pos += cnt;
+            printf("read %d bytes\n", cnt);
+        }
+        if(in->revents & POLLERR){
+            printf("POLLERR on in\n");
+            buf->in_died = true;
+        }
+    }
+    return 1;
+}
+
+int push_data(pollfd* out, s_buffer* buf){
+    printf("starting push\n");
+    if(buf->out_died) return 0;
+    if(buf->pos > 0){
+        int cnt = write(out->fd, buf->data, buf->pos);
+        if(cnt <= 0){
+            buf->out_died = true;
+            perror("write() failed");
+            return 0;
+        }
+        if(cnt > 0){
+            memmove(buf->data, buf->data + cnt, buf->pos - cnt);
+        }
+        buf->pos -= cnt;
+    }
+    if(buf->in_died && buf->pos == 0){
+        printf("POLLERR on out\n");
+        buf->out_died = true;
+    }
+    printf("buff pos = %d\n", buf->pos);
+    return 1;
 }
 
 s_buffer* allocate_buffer(int tag){
-
+    s_buffer* buf = (s_buffer*)malloc(sizeof(s_buffer));
+    buf->tag = tag;
+    buf->data = (char*)malloc(BUF_SIZE);
+    buf->pos = 0;
+    buf->out_died = false;
+    buf->in_died = false;
+    return buf;
 }
 
 void dealloc_buffer(s_buffer* buf){
-
+    printf("deallocating buffer %s\n", buf->data);
+    free(buf->data);
+    free(buf);
 }
 
 void init_network(){
@@ -43,9 +103,8 @@ void init_network(){
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
-    if (!getaddrinfo(NULL, PORT, &hints, &srvinfo)){
+    if (getaddrinfo(NULL, PORT, &hints, &srvinfo) != 0){
         perror("getaddrinfo() failed");
-        exit(1);
     }
     while(srvinfo){
         fds.push_back(socket(srvinfo->ai_family, srvinfo->ai_socktype, srvinfo->ai_protocol));
@@ -54,35 +113,51 @@ void init_network(){
         setsockopt(fds.back(), SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
         listen(fds.back(), 5);
         srvinfo = srvinfo->ai_next;
+        printf("initializes socket %d\n", fds.back());
     }
     freeaddrinfo(srvinfo);
 
 
 }
 
-void handle_sender(int fd){
-    printf("handling sender: %d\n", fd);
-
-}
-
-void handle_recvr(int fd){
-    printf("handling recvr: %d\n", fd);
-}
 
 void handle_client(int fd){
-    char[5] cmd;
+    printf("handling client: %d\n", fd);
+    char cmd[5];
     read(fd, cmd, 5);
-    if(!strcmp(cmd, "send"))
-        handle_sender(fd);
-    else if(!strcmp(cmd, "recv"))
-        handle_recvr(fd);
+    pollfd client;
+    if(!strcmp(cmd, "send")){
+        printf("sender connectedi\n");
+        client.fd = fd;
+        client.events = POLLIN | POLLERR;
+        polls.push_back(client);
+        ids.push_back(ID);
+        senders.push_back(true);
+        buffers[ID] = allocate_buffer(ID);
+        memset(cmd, 0, 5);
+        sprintf(cmd, "%d", ID);
+        ID++;
+        write(fd, cmd, 5);
+    }
+    else if(!strcmp(cmd, "recv")){
+        printf("recvr connected\n");
+        client.fd = fd;
+        client.events = POLLOUT | POLLERR;
+        polls.push_back(client);
+        read(fd, cmd, 5);
+        int id = atoi(cmd);
+        ids.push_back(id);
+        senders.push_back(false);
+    }
     else {
         close(fd);
-        pirntf("unknown command: %s\n", cmd);
+        printf("unknown command: %s\n", cmd);
+        return;
     }
 }
 
 void main_srv(){
+    init_network();
     vector<pollfd> pollfds(fds.size());
     for(int i = 0; i < pollfds.size(); ++i){
         pollfds[i].fd = fds[i];
@@ -97,10 +172,10 @@ void main_srv(){
         int status;
         wait3(&status, WNOHANG, NULL);
         if(ret == 0) continue;
-        for(int i = 0; i < pollfds.size(), ++i){
+        for(int i = 0; i < pollfds.size(); ++i){
             if(pollfds[i].revents & (POLLERR | POLLHUP | POLLRDHUP | POLLNVAL)){
                 pollfds[i].events = 0;
-                close(fds[i]);
+                close(pollfds[i].fd);
                 perror("socket has been disconnected");
             }
             if(pollfds[i].revents & POLLIN){
@@ -110,21 +185,54 @@ void main_srv(){
                     continue;
                 }
                 printf("accepting %d\n", cfd);
-                int pid = fork();
-                if(pid = 0){
-                    for(int i = 0; i < fds.size(); ++i){
-                        close(fds[i]);
-                    }
-                    handle_client(cfd);
-                    return;
+                handle_client(cfd);
+            }
+        }
+        int sret = poll(polls.data(), polls.size(), 1);
+        if(sret < 0){
+            perror("polls() error");
+            exit(1);
+        }
+        if(sret == 0) continue;
+        for(int i = 0; i < polls.size(); ++i){
+            if(polls[i].revents & POLLERR){
+                printf("socket disconnected: %d\n", polls[i].fd);
+                //close(polls[i].fd);
+                //continue;
+            }
+            if(senders[i]){
+                if(polls[i].revents){
+                    printf("pulling data from sender id: %d\n", ids[i]);
+                    pull_data(&polls[i], buffers[ids[i]]);
                 }
-                close(cfd);
+            } else {
+                if(polls[i].revents){
+                    printf("pushing data\n");
+                    push_data(&polls[i], buffers[ids[i]]);
+                }
+            }
+        }
+        for(int i = 0; i < polls.size(); ++i){
+            if(senders[i]){
+                if(buffers[ids[i]]->in_died && buffers[ids[i]]->out_died){
+                    printf("sending finished6 %d\n", ids[i]);
+                    polls[i].events = 0;
+                    close(polls[i].fd);
+                }
+            } else {
+                if(buffers[ids[i]]->out_died){
+                    printf("recving finished: %d\n", ids[i]);
+                    polls[i].events = 0;
+                   // if(buffers[ids[i]] != 0)
+                       // dealloc_buffer(buffers[ids[i]]);
+                    close(polls[i].fd);
+                }
             }
         }
     }
 }
 
 int main(int argc, char** argv){
-
+    main_srv();
 return 0;
 }
